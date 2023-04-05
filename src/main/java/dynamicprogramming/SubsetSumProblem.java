13f9ed6d488a30368df3be4065b66e384c23efcd
Lokesh Singh <ashawat.lokesh@gmail.com>
2023-04-06 02:04:57 +0530

    Minimum sum partition

--- src/main/java/dynamicprogramming/SubsetSumProblem.java
+++ src/main/java/dynamicprogramming/SubsetSumProblem.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SubsetSumProblem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         int n = 5;
         int[] arr = new int[n];
         for(int i = 0; i<n; i++ ){
