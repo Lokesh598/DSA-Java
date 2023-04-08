33f76e7fc9bdb66b0695659970ec4c32e7f08b88
Lokesh Singh <ashawat.lokesh@gmail.com>
2023-04-08 17:51:11 +0530

    lcs algorithm

---
+++ src/main/java/dynamicprogramming/longestcommonsubsequence/lcs/LCSTabulationOrTopDown.java
@@ -0,0 +1,31 @@
+package dynamicprogramming.longestcommonsubsequence.lcs;
+
+public class LCSTabulationOrTopDown {
+    static int[][] tb;
+    public static int lcsTabulation(int x, int y, String s1, String s2) {
+        tb = new int[x+1][y+1];
+        for(int i = 0; i<x+1; i++) {
+            for(int j = 0; j<y+1; j++) {
+                if(i == 0 || y == 0)
+                    tb[i][j] = 0;
+            }
+        }
+        for(int i = 1; i<x+1; i++) {
+            for (int j = 1; j < y + 1; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    tb[i][j] = 1 + tb[i-1][j-1];
+                } else {
+                    tb[i][j] = Math.max(tb[i][j-1], tb[i-1][j]);
+                }
+            }
+        }
+        return tb[x][y];
+    }
+    public static void main(String[] args) {
+        String s1 = "ABCDGH";
+        String s2 = "AEDFHR";
+
+        int res = lcsTabulation(s1.length(), s2.length(), s1, s2);
+        System.out.println(res);
+    }
+}
