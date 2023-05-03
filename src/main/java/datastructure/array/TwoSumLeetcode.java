70c459865976ebfb9cdc8f2cbb0882a728ecfa17
Lokesh Singh <ashawat.lokesh@gmail.com>
2023-05-04 01:34:29 +0530

    two sum proble

--- src/main/java/datastructure/array/TwoSumLeetcode.java
+++ src/main/java/datastructure/array/TwoSumLeetcode.java
@@ -1,5 +1,39 @@
 package datastructure.array;
 
-public class TwoSumLeetcode {
+import java.util.Arrays;
 
+/**
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ */
+public class TwoSumLeetcode {
+    public static int[] twoSum(int[] arr, int target) {
+        int[] res = new int[arr.length];
+        int left = 0, right = arr.length-1;
+        Arrays.sort(arr);
+
+        while (left < right) {
+            if (left != 0 && arr[left] == arr[left-1]) {
+                left++;
+                continue;
+            }
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                res[left] = left;
+                res[right] = right;
+                left++;
+                right --;
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {3,2,4};
+        System.out.println(Arrays.toString(twoSum(arr, 6)));
+    }
 }
